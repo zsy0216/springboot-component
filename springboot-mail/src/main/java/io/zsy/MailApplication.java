da33fc5e08c92d31f0b388fd83a624bc809332db
zsy0216 <594983498@qq.com>
2020-08-14 17:15:20 +0800

    feat: start mail project

---
+++ springboot-mail/src/main/java/io/zsy/MailApplication.java
@@ -0,0 +1,16 @@
+package io.zsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shuaiyin.zhang
+ * @description
+ * @date 2020/08/14
+ */
+@SpringBootApplication
+public class MailApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MailApplication.class, args);
+	}
+}
