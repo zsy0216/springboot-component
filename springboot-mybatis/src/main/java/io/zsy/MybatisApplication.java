64da79d0cd329b93f039b5d2c20f72b94a1094ad
zsy0216 <594983498@qq.com>
2021-06-24 14:22:57 +0800

    feat: mybatis-boot init

---
+++ springboot-mybatis/src/main/java/io/zsy/MybatisApplication.java
@@ -0,0 +1,15 @@
+package io.zsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangshuaiyin
+ * @date 2021-06-24 14:00
+ */
+@SpringBootApplication
+public class MybatisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class, args);
+    }
+}
---
+++ springboot-mybatis/src/test/java/io/zsy/MyBatisTests.java
@@ -0,0 +1,22 @@
+package io.zsy;
+
+import io.zsy.mapper.MenuMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+/**
+ * @author zhangshuaiyin
+ * @date 2021-06-24 13:57
+ */
+@SpringBootTest
+public class MyBatisTests {
+    @Resource
+    MenuMapper menuMapper;
+
+    @Test
+    public void test() {
+        menuMapper.selectList().forEach(System.out::println);
+    }
+}
